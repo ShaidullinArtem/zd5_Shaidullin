2ea7e61b190954be4efcd590d87e3d6a3a0e1050
Zero-i00 <clean.shiro@gmail.com>
2023-06-22 00:43:56 +0500

    Добавьте файлы проекта.

---
+++ zd5_Shaidullin/zd5_Shaidullin/Login.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zd5_Shaidullin.store;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zd5_Shaidullin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void signin_Clicked(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(login_field.Text) || string.IsNullOrEmpty(password_field.Text))
+            {
+                DisplayAlert("Ошибка", "Заполните все поля", "Окей");
+                return;
+            }
+
+            await SaveApplicationProperty("user", login_field.Text);
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private async Task SaveApplicationProperty<T>(string key, T value)
+        {
+            Xamarin.Forms.Application.Current.Properties[key] = value;
+            await Xamarin.Forms.Application.Current.SavePropertiesAsync();
+        }
+    }
+}
---
+++ zd5_Shaidullin/zd5_Shaidullin/store/Store.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd5_Shaidullin.store
+{
+    public class Store
+    {
+        public async Task SaveApplicationProperty<T>(string key, T value)
+        {
+            Xamarin.Forms.Application.Current.Properties[key] = value;
+            await Xamarin.Forms.Application.Current.SavePropertiesAsync();
+        }
+
+       public T LoadApplicationProperty<T>(string key)
+        {
+            return (T)Xamarin.Forms.Application.Current.Properties[key];
+        }
+    }
+}
